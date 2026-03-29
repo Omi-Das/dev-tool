bf2d23948447c38beadd55dfedc6d472ad8459cd
Omi-Das <omi06133@gmail.com>
2026-03-30 03:12:16 +0600

    Navbar Almost Complete

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,48 +7,48 @@ const Navbar = () => {
     //         Omi Das is a student 
     //     </div>
     //   </div>
-  <div className="navbar bg-base-100 p-2 md:p-2 h-10px shadow-sm mx-auto">
+  <div className="navbar bg-base-100 h-10px shadow-sm mx-auto flex justify-between items-center pl-6 pr-6">
   {/* Left Side: Brand Name */}
-  <div className="flex-1">
+  <div className="pl-6">
     <a className="text-2xl font-bold text-[#6333FF] cursor-pointer">DigiTools</a>
   </div>
 
   {/* Middle: Navigation Links (Hidden on small screens) */}
-  <div className="">
-    <ul className="menu menu-horizontal px-1 gap-4 font-medium text-gray-600">
-      <li><a className="hover:text-[#6333FF]">Products</a></li>
-      <li><a className="hover:text-[#6333FF]">Features</a></li>
-      <li><a className="hover:text-[#6333FF]">Pricing</a></li>
-      <li><a className="hover:text-[#6333FF]">Testimonials</a></li>
-      <li><a className="hover:text-[#6333FF]">FAQ</a></li>
+  <div className="flex justify-between items-center hidden lg:block">
+    <ul className="menu menu-horizontal px-1 gap-4 font-medium text-gray-600 pl-90">
+      <li><a href='/' className="hover:text-[#6333FF]">Products</a></li>
+      <li><a href='/' className="hover:text-[#6333FF]">Features</a></li>
+      <li><a href='/' className="hover:text-[#6333FF]">Pricing</a></li>
+      <li><a href='/' className="hover:text-[#6333FF]">Testimonials</a></li>
+      <li><a href='/'className="hover:text-[#6333FF]">FAQ</a></li>
     </ul>
   </div>
 
   {/* Right Side: Cart, Login & CTA */}
-  <div className='grid grid-cols-1'>
-    <div className="flex-none gap-4 items-center ml-4">
-    {/* Shopping Cart Icon */}
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        <div className="indicator">
-         <img src={shoppingCart} alt="shoppingCart" />
-        </div>
+  <div className="flex items-center justify-end gap-4 ml-auto"> 
+  {/* shooping cart icon*/}
+  <div className="dropdown dropdown-end">
+    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+      <div className="indicator">
+    {/* shopping cart image jate chutu boro na hoi soman rakhlam
+     */}
+        <img src={shoppingCart} alt="shoppingCart" className="w-6 h-6" />
+           <span class="badge badge-sm indicator-item">0</span>
       </div>
     </div>
-
-    {/* Login Link */}
-   <div>
-     <a className="hidden sm:block font-medium text-gray-700 hover:text-black cursor-pointer">Login</a>
-   </div>
-
-    {/* Get Started Button */}
-   <div>
-     <button className="btn bg-[#6333FF] hover:bg-[#4f28cc] text-white border-none rounded-full px-6">
-      Get Started
-    </button>
-   </div>
   </div>
-  </div>
+
+  {/* login link*/}
+  <a className="hidden sm:block font-medium text-gray-700 hover:text-black cursor-pointer">
+    Login
+  </a>
+
+  {/* get started button */}
+  <button className="btn bg-[#6333FF] hover:bg-[#4f28cc] text-white border-none rounded-full px-6 min-h-0 h-10">
+    Get Started
+  </button>
+</div>
+
 </div>
 
    
