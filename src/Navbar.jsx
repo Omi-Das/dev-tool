d978b26d7b8557f0e187df6cc4f9703f7308e862
Omi-Das <omi06133@gmail.com>
2026-03-30 02:06:07 +0600

    first tailwind set up

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+      <div className="w-[800px]">
+          <div className='text-center text-white bg-amber-700'>
+            Omi Das is a student 
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
